0004edf9a2187a441936af5fbda484c901d9f708
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-11-30 19:52:52 +0300

    feat: add vitest

--- src/shared/helpers/cropFileNameExtension.ts
+++ src/shared/helpers/cropFileNameExtension.ts
@@ -1,3 +1,9 @@
 export function cropFileNameExtension(fileName: string): string {
-  return fileName.split('.').slice(0, -1).join('.')
+  const splitString = fileName.split('.').filter(value => value !== '')
+
+  if (splitString.length <= 1) {
+    return fileName
+  }
+
+  return splitString.slice(0, -1).join('.')
 }
