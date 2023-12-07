bd606258ef6fc312f751854fb684ae82b2e4c931
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-12-07 14:28:18 +0300

    feat: add ButtonRemoveSettings

--- src/components/SettingsPanel/Toolbar/Toolbar.tsx
+++ src/components/SettingsPanel/Toolbar/Toolbar.tsx
@@ -1,6 +1,7 @@
 import { Flex, type PaddingProps } from '@radix-ui/themes'
 
 import { ButtonReset } from '@components/ButtonReset'
+import { ButtonRemoveSettings } from '@components/ButtonRemoveSettings'
 import { useResetSettings } from '@stores/hooks/useResetSettings'
 import styles from './Toolbar.module.css'
 
@@ -19,8 +20,9 @@ export function Toolbar() {
   const { handleReset } = useResetSettings()
 
   return (
-    <Flex align='center' justify='end' {...padding} className={styles.root}>
+    <Flex align='center' justify='end' gap='1' {...padding} className={styles.root}>
       <ButtonReset title='Reset' tooltipTitle='Reset all settings' onClick={handleReset} />
+      <ButtonRemoveSettings />
     </Flex>
   )
 }
