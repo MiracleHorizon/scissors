499c4348c774730b4e288d2eeb8deea54fcac916
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-12-09 20:13:34 +0300

    chore: update MAX_FILE_SIZE_MB

--- src/shared/helpers/isValidFileSize.ts
+++ src/shared/helpers/isValidFileSize.ts
@@ -1,6 +1,6 @@
 const BYTES_IN_KB = 1024
 export const BYTES_IN_MB = BYTES_IN_KB * 1024
-export const MAX_FILE_SIZE_MB = 10
+export const MAX_FILE_SIZE_MB = 4.5
 export const MAX_FILE_SIZE = MAX_FILE_SIZE_MB * BYTES_IN_MB
 
 export function isValidFileSize(file: File): boolean {
