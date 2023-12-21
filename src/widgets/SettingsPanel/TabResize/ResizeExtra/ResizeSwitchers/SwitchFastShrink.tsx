dda76c3c4b8066078dea900d1c3b9246e1c1821c
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-12-21 20:12:22 +0300

    fix: to use the "fast shrink" option in "resize", need to select JPEG and WebP as the convert format

--- src/widgets/SettingsPanel/TabResize/ResizeExtra/ResizeSwitchers/SwitchFastShrink.tsx
+++ src/widgets/SettingsPanel/TabResize/ResizeExtra/ResizeSwitchers/SwitchFastShrink.tsx
@@ -4,12 +4,21 @@ import { useCallback } from 'react'
 
 import { OptionSwitch } from '@widgets/SettingsPanel/OptionSwitch'
 import { useResizeStore } from '@stores/resize'
+import { useFormatStore } from '@stores/format'
+import { ConvertFormat } from '@libs/Sharp'
+
+const ALLOWED_FORMATS_FOR_FAST_SHRINK = [ConvertFormat.JPEG, ConvertFormat.WEBP]
 
 export function SwitchFastShrink() {
+  const format = useFormatStore(state => state.format)
   const fastShrink = useResizeStore(state => state?.fastShrinkOnLoad)
   const toggleFastShrink = useResizeStore(state => state.toggleFastShrink)
 
   const handleToggleFastShrink = useCallback(() => toggleFastShrink(), [toggleFastShrink])
+
+  if (!format || !ALLOWED_FORMATS_FOR_FAST_SHRINK.includes(format)) {
+    return null
+  }
 
   return <OptionSwitch checked={fastShrink} title='Fast shrink' onClick={handleToggleFastShrink} />
 }
