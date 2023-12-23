8f069e2490b3a0515fff46945921d8f1d3a58d8e
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-12-23 17:41:16 +0300

    chore: update ButtonFileUpload icon and appearance

--- src/components/uploading/ButtonFileUpload.tsx
+++ src/components/uploading/ButtonFileUpload.tsx
@@ -1,18 +1,20 @@
 'use client'
 
 import { IconButton } from '@radix-ui/themes'
-import { FilePlusIcon } from '@radix-ui/react-icons'
+import { UploadIcon } from '@radix-ui/react-icons'
 
 import { type ComponentProps, withFileUploader } from '@hoc/withFileUploader'
 
 function ButtonFileUpload({ children, onClick }: Pick<ComponentProps, 'children' | 'onClick'>) {
   return (
-    <IconButton size='2' variant='surface' onClick={onClick}>
-      <FilePlusIcon width='20px' height='20px' />
+    <IconButton size='2' color='gray' variant='outline' onClick={onClick}>
+      <UploadIcon width='22px' height='22px' />
       {children}
     </IconButton>
   )
 }
+
+ButtonFileUpload.displayName = 'ButtonFileUpload'
 
 const Button = withFileUploader(ButtonFileUpload)
 
