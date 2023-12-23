89696e92bc2bdc00d8f0f66fa7ad9f888763ca9e
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-12-23 17:41:42 +0300

    chore: add variant props to the ButtonDelete

--- src/lib/ui/ButtonDelete.tsx
+++ src/lib/ui/ButtonDelete.tsx
@@ -4,15 +4,20 @@ import { forwardRef } from 'react'
 import { IconButton, type MarginProps } from '@radix-ui/themes'
 import { TrashIcon } from '@radix-ui/react-icons'
 
-export const ButtonDelete = forwardRef<HTMLButtonElement, Props>((props: Props, ref) => (
-  <IconButton ref={ref} size='2' {...props}>
-    <TrashIcon width='24px' height='24px' />
-  </IconButton>
-))
+import type { ButtonVariant } from '@lib/theme'
+
+export const ButtonDelete = forwardRef<HTMLButtonElement, Props>(
+  ({ variant, ...props }: Props, ref) => (
+    <IconButton variant={variant ?? 'solid'} ref={ref} size='2' {...props}>
+      <TrashIcon width='24px' height='24px' />
+    </IconButton>
+  )
+)
 
 ButtonDelete.displayName = 'ButtonDelete'
 
 interface Props extends MarginProps {
   onClick: VoidFunction
+  variant?: ButtonVariant
   disabled?: boolean
 }
