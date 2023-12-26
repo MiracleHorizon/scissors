34ea7d4c8ebd802419f4da25e70b1ead0de4637f
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-12-26 15:57:21 +0300

    chore: fix eslint arrow-body rule for the OptionCheckbox

--- src/widgets/SettingsPanel/OptionCheckbox.tsx
+++ src/widgets/SettingsPanel/OptionCheckbox.tsx
@@ -1,18 +1,16 @@
 import { memo } from 'react'
 import { Checkbox, Flex, Text } from '@radix-ui/themes'
 
-export const OptionCheckbox = memo(({ title, ...props }: Props) => {
-  return (
-    <Text as='label' size='2' title={title}>
-      <Flex align='center' gap='2'>
-        <Checkbox size='3' {...props} />
-        <Text as='span' className='truncate'>
-          {title}
-        </Text>
-      </Flex>
-    </Text>
-  )
-})
+export const OptionCheckbox = memo(({ title, ...props }: Props) => (
+  <Text as='label' size='2' title={title}>
+    <Flex align='center' gap='2'>
+      <Checkbox size='3' {...props} />
+      <Text as='span' className='truncate'>
+        {title}
+      </Text>
+    </Flex>
+  </Text>
+))
 
 OptionCheckbox.displayName = 'OptionCheckbox'
 
