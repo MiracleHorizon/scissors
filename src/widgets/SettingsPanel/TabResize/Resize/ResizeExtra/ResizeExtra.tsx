202bbda9f10535f434964fe2c6ce2855492f99e0
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-12-26 16:21:06 +0300

    refactor(resize): move extra selects to the ResizeSelectList

--- src/widgets/SettingsPanel/TabResize/Resize/ResizeExtra/ResizeExtra.tsx
+++ src/widgets/SettingsPanel/TabResize/Resize/ResizeExtra/ResizeExtra.tsx
@@ -3,35 +3,19 @@
 import { Flex } from '@radix-ui/themes'
 
 import { ResizeBackgroundPopover } from './ResizeBackgroundPopover'
-import { SelectResizeFit } from './SelectResizeFit'
-import { SelectResizePosition } from './SelectResizePosition'
-import { SelectResizeKernel } from './SelectResizeKernel'
+
 import { ResizeCheckboxes } from './ResizeCheckboxes'
+import { ResizeSelectList } from './ResizeSelectList'
 import { useResizeStore } from '@stores/resize'
 
 export function ResizeExtra() {
-  const fit = useResizeStore(state => state.fit)
-  const kernel = useResizeStore(state => state.kernel)
-  const position = useResizeStore(state => state.position)
   const background = useResizeStore(state => state.background)
 
   return (
     <Flex direction='column' gap='2' width='100%'>
-      <Flex
-        direction={{
-          initial: 'column',
-          xs: 'row'
-        }}
-        gap='2'
-      >
-        {fit && <SelectResizeFit />}
-        {kernel && <SelectResizeKernel />}
-        {position && <SelectResizePosition />}
-      </Flex>
-
+      <ResizeSelectList />
       {background && <ResizeBackgroundPopover background={background} />}
-
-      <ResizeCheckboxes mt='3' />
+      <ResizeCheckboxes mt='2' />
     </Flex>
   )
 }
