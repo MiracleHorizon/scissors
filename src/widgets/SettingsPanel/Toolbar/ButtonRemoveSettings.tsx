ec3d34db8c2920f8afe284d0397e835f37de954a
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-12-27 19:12:15 +0300

    fix: remove unused Tooltip in ButtonRemoveSettings
    [skip ci]

--- src/widgets/SettingsPanel/Toolbar/ButtonRemoveSettings.tsx
+++ src/widgets/SettingsPanel/Toolbar/ButtonRemoveSettings.tsx
@@ -1,16 +1,10 @@
 'use client'
 
-import { Tooltip } from '@radix-ui/themes'
-
 import { useRemoveSettings } from '@stores/hooks/useRemoveSettings'
 import { ButtonDelete } from '@ui/ButtonDelete.tsx'
 
 export function ButtonRemoveSettings() {
   const { handleRemove } = useRemoveSettings()
 
-  return (
-    <Tooltip delayDuration={900} content='Remove all settings'>
-      <ButtonDelete onClick={handleRemove} />
-    </Tooltip>
-  )
+  return <ButtonDelete tooltipContent='Remove All Settings' onClick={handleRemove} />
 }
