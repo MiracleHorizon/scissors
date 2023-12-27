30da5ddb5c16d838613df69bfe5c75680ea32817
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-12-27 19:06:28 +0300

    refactor: use ButtonDelete in ButtonRemoveSettings instead of local IconButton usage

--- src/lib/ui/ButtonDelete.tsx
+++ src/lib/ui/ButtonDelete.tsx
@@ -1,23 +1,41 @@
 'use client'
 
-import { forwardRef } from 'react'
-import { IconButton, type MarginProps } from '@radix-ui/themes'
+import { memo } from 'react'
+import { IconButton, type MarginProps, Tooltip } from '@radix-ui/themes'
 import { TrashIcon } from '@radix-ui/react-icons'
 
-import type { ButtonVariant } from '@lib/theme'
+import { isTooltipOpen } from '@helpers/isTooltipOpen'
+import type { ButtonProps } from '@lib/theme'
 
-export const ButtonDelete = forwardRef<HTMLButtonElement, Props>(
-  ({ variant, ...props }: Props, ref) => (
-    <IconButton variant={variant ?? 'solid'} ref={ref} size='2' {...props}>
+export const ButtonDelete = memo(({ tooltipContent, disabled, ...props }: Props) => {
+  const Button = (
+    <IconButton {...props}>
       <TrashIcon width='24px' height='24px' />
     </IconButton>
   )
-)
+
+  if (!tooltipContent) {
+    return Button
+  }
+
+  return (
+    <Tooltip
+      delayDuration={800}
+      open={isTooltipOpen({
+        content: tooltipContent,
+        isParentDisabled: disabled
+      })}
+      content={tooltipContent}
+    >
+      {Button}
+    </Tooltip>
+  )
+})
 
 ButtonDelete.displayName = 'ButtonDelete'
 
-interface Props extends MarginProps {
-  onClick?: VoidFunction
-  variant?: ButtonVariant
+interface Props extends ButtonProps, MarginProps {
+  tooltipContent?: string
   disabled?: boolean
+  onClick?: VoidFunction
 }
--- src/widgets/SettingsPanel/Toolbar/ButtonRemoveSettings.tsx
+++ src/widgets/SettingsPanel/Toolbar/ButtonRemoveSettings.tsx
@@ -1,22 +1,16 @@
 'use client'
 
-import { IconButton, Tooltip } from '@radix-ui/themes'
-import { TrashIcon } from '@radix-ui/react-icons'
+import { Tooltip } from '@radix-ui/themes'
 
 import { useRemoveSettings } from '@stores/hooks/useRemoveSettings'
-
-const iconStyle = {
-  marginTop: '1px'
-}
+import { ButtonDelete } from '@ui/ButtonDelete.tsx'
 
 export function ButtonRemoveSettings() {
   const { handleRemove } = useRemoveSettings()
 
   return (
     <Tooltip delayDuration={900} content='Remove all settings'>
-      <IconButton size='2' onClick={handleRemove}>
-        <TrashIcon width='20px' height='20px' style={iconStyle} />
-      </IconButton>
+      <ButtonDelete onClick={handleRemove} />
     </Tooltip>
   )
 }
