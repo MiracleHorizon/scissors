22adae335d7ac3f9ae960dc6accc609055cc1778
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-12-28 20:14:57 +0300

    chore(ui): add tooltip for the BadgeBeta

--- src/lib/ui/badges/BadgeBeta.tsx
+++ src/lib/ui/badges/BadgeBeta.tsx
@@ -1,13 +1,15 @@
-import { Badge, type MarginProps } from '@radix-ui/themes'
+import { Badge, type MarginProps, Tooltip } from '@radix-ui/themes'
 
 import type { BadgeProps } from '@lib/theme'
 import type { ClassNameProps } from '@app-types/ClassNameProps'
 
 export function BadgeBeta(props: Props) {
   return (
-    <Badge size='1' color='orange' {...props}>
-      Beta
-    </Badge>
+    <Tooltip content='This feature is in beta. It may not work as expected'>
+      <Badge size='1' color='orange' {...props}>
+        Beta
+      </Badge>
+    </Tooltip>
   )
 }
 
