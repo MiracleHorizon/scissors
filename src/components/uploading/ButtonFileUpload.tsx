30d64c62a896089d8fc8392a8a252a9d536f558d
MiracleHorizon <alexeenkodenisjke@gmail.com>
2023-12-28 20:31:19 +0300

    chore: add tooltip for the ButtonFileUpload

--- src/components/uploading/ButtonFileUpload.tsx
+++ src/components/uploading/ButtonFileUpload.tsx
@@ -1,16 +1,18 @@
 'use client'
 
-import { IconButton } from '@radix-ui/themes'
+import { IconButton, Tooltip } from '@radix-ui/themes'
 import { UploadIcon } from '@radix-ui/react-icons'
 
 import { type ComponentProps, withFileUploader } from '@hoc/withFileUploader'
 
 function ButtonFileUpload({ children, onClick }: Pick<ComponentProps, 'children' | 'onClick'>) {
   return (
-    <IconButton size='2' color='gray' variant='outline' onClick={onClick}>
-      <UploadIcon width='20px' height='20px' />
-      {children}
-    </IconButton>
+    <Tooltip content='Upload New File'>
+      <IconButton size='2' color='gray' variant='outline' onClick={onClick}>
+        <UploadIcon width='20px' height='20px' />
+        {children}
+      </IconButton>
+    </Tooltip>
   )
 }
 
