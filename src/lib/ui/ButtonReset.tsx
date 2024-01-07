0e34cf8434401faa0aafda235575d275fe9d5b29
MiracleHorizon <alexeenkodenisjke@gmail.com>
2024-01-07 18:48:59 +0300

    chore(ui): add default button props values for the ButtonReset
    [skip ci]

--- src/lib/ui/ButtonReset.tsx
+++ src/lib/ui/ButtonReset.tsx
@@ -9,7 +9,7 @@ import type { ButtonProps } from '@lib/theme'
 import type { ClassNameProps } from '@app-types/ClassNameProps'
 
 export const ButtonReset = forwardRef<HTMLButtonElement, Props>(
-  ({ tooltipContent, disabled, ...props }, ref) => (
+  ({ tooltipContent, disabled, variant = 'outline', color = 'red', ...props }, ref) => (
     <Tooltip
       delayDuration={800}
       open={isTooltipOpen({
@@ -18,7 +18,7 @@ export const ButtonReset = forwardRef<HTMLButtonElement, Props>(
       })}
       content={tooltipContent}
     >
-      <IconButton ref={ref} {...props} disabled={disabled}>
+      <IconButton ref={ref} {...props} color={color} variant={variant} disabled={disabled}>
         <ResetIcon width='18px' height='18px' />
       </IconButton>
     </Tooltip>
