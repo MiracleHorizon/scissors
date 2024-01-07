bbdc7bc5040196dbcb7c78b27f8ead7653064f50
MiracleHorizon <alexeenkodenisjke@gmail.com>
2024-01-07 05:52:01 +0300

    fix: correct random hex color generation

--- src/lib/helpers/colors/getRandomHexColor.test.ts
+++ src/lib/helpers/colors/getRandomHexColor.test.ts
@@ -5,7 +5,7 @@ import { hexValidationRegex } from './regular-expressions'
 
 describe('@lib/helpers/colors/getRandomHexColor', () => {
   it('should return a random hex color', () => {
-    for (let i = 0; i < 10; i++) {
+    for (let i = 0; i < 15; i++) {
       expect(getRandomHexColor()).toMatch(hexValidationRegex)
     }
   })
--- src/lib/helpers/colors/getRandomHexColor.ts
+++ src/lib/helpers/colors/getRandomHexColor.ts
@@ -1,8 +1,10 @@
 export function getRandomHexColor(): string {
   const WHITE_COLOR_NUMBER = 16777215
+  const MAX_HEX_COLOR_LENGTH = 6
 
-  const randomNumber = Math.floor(Math.random() * WHITE_COLOR_NUMBER)
-  const randomHexColor = randomNumber.toString(16)
+  const randomColor = Math.round(WHITE_COLOR_NUMBER * Math.random()).toString(16)
+  const paddingLength = MAX_HEX_COLOR_LENGTH - randomColor.length
+  const paddingZeros = '000000'
 
-  return '#' + randomHexColor
+  return '#' + paddingZeros.substring(0, paddingLength) + randomColor
 }
