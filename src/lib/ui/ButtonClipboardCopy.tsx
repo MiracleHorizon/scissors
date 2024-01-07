ad2ffbec817a8c1203249df775defd8308c23961
MiracleHorizon <alexeenkodenisjke@gmail.com>
2024-01-07 19:27:33 +0300

    refactor(ui): update ButtonClipboardCopy icon, copy action and styles

--- src/lib/ui/ButtonClipboardCopy.tsx
+++ src/lib/ui/ButtonClipboardCopy.tsx
@@ -4,14 +4,17 @@ import { useEffect, useState } from 'react'
 import { IconButton, Tooltip } from '@radix-ui/themes'
 
 import { CopyIcon } from '@ui/icons/CopyIcon'
-import { CopiedIcon } from '@ui/icons/CopiedIcon'
+import { CheckIcon } from '@ui/icons/CheckIcon'
 import { copyToClipboard } from '@helpers/copyToClipboard'
 import type { ButtonProps } from '@lib/theme'
 
 export function ButtonClipboardCopy({ copyValue, size, variant = 'ghost', color = 'gray' }: Props) {
   const [isCopied, setIsCopied] = useState(false)
 
-  const handleCopy = () => copyToClipboard(copyValue).then(() => setIsCopied(true))
+  const handleCopy = () => {
+    if (isCopied) return
+    copyToClipboard(copyValue).then(() => setIsCopied(true))
+  }
 
   useEffect(() => {
     if (!isCopied) return
@@ -23,9 +26,15 @@ export function ButtonClipboardCopy({ copyValue, size, variant = 'ghost', color 
 
   return (
     <Tooltip content={isCopied ? 'Copied!' : 'Copy to clipboard'}>
-      <IconButton size={size} variant={variant} color={color} radius='large' onClick={handleCopy}>
+      <IconButton
+        size={size}
+        variant={isCopied ? 'surface' : variant}
+        color={isCopied ? 'blue' : color}
+        radius='large'
+        onClick={handleCopy}
+      >
         {isCopied ? (
-          <CopiedIcon width='20px' height='20px' />
+          <CheckIcon width='24px' height='24px' color='var(--accent-9)' />
         ) : (
           <CopyIcon width='20px' height='20px' />
         )}
