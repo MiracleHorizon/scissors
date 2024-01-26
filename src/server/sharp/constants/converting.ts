6c1f4ae75fdd0c8bb2a95f958a0227dc03534d4e
MiracleHorizon <alexeenkodenisjke@gmail.com>
2024-01-26 16:45:53 +0300

    chore: update "normalise" option min / max / default values

--- src/server/sharp/constants/converting.ts
+++ src/server/sharp/constants/converting.ts
@@ -5,7 +5,7 @@ export const IMAGE_FILE_FORMAT = {
   JPG: 'jpg',
   PNG: 'png',
   WEBP: 'webp'
-}
+} as const
 export const ALLOWED_IMAGE_FORMATS = Object.values(IMAGE_FILE_FORMAT)
   .map(format => `image/${format}`)
   .join(', ')
@@ -16,12 +16,14 @@ export const DEFAULT_GRAYSCALE = false
 export const DEFAULT_TINT_COLOR = '#000000'
 
 // Normalise
-export const MIN_NORMALISE = 1
-export const MAX_NORMALISE = 99
+export const MIN_NORMALISE = 0
+export const MAX_NORMALISE = 100
+export const DEFAULT_LOWER_NORMALISE = 1
+export const DEFAULT_UPPER_NORMALISE = 99
 export const DEFAULT_NORMALISE: NormaliseOptions = {
-  lower: MIN_NORMALISE,
-  upper: MAX_NORMALISE
-}
+  lower: DEFAULT_LOWER_NORMALISE,
+  upper: DEFAULT_UPPER_NORMALISE
+} as const
 
 // Blur
 export const MIN_BLUR_SIGMA = 0.3
@@ -30,7 +32,7 @@ export const BLUR_SIGMA_STEP = 0.1
 export const DEFAULT_BLUR: BlurOptions = {
   value: false,
   sigma: null
-}
+} as const
 
 // Negate
 export const DEFAULT_NEGATE_VALUE = false
@@ -38,7 +40,7 @@ export const DEFAULT_NEGATE_ALPHA = false
 export const DEFAULT_NEGATE: NegateOptions = {
   value: DEFAULT_NEGATE_VALUE,
   alpha: DEFAULT_NEGATE_ALPHA
-}
+} as const
 
 // Gamma
 export const MIN_GAMMA = 1
