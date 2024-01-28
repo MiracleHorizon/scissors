dda0d1d163b7917624bd6f9efdd898fb9db2063e
MiracleHorizon <alexeenkodenisjke@gmail.com>
2024-01-28 15:00:53 +0300

    feat: add dynamic import for Toolbar for correct user tour highlighting

--- src/widgets/SettingsPanel/Toolbar/Toolbar.tsx
+++ src/widgets/SettingsPanel/Toolbar/Toolbar.tsx
@@ -1,15 +1,33 @@
+import dynamic from 'next/dynamic'
+import MediaQuery from 'react-responsive'
 import { Flex } from '@radix-ui/themes'
 
 import { ToolbarTabList } from './ToolbarTabList'
-import { ToolbarContent } from './ToolbarContent'
-import { ToolbarDropdownMenu } from './ToolbarDropdownMenu'
+import { ToolbarContentSkeleton, ToolbarDropdownMenuSkeleton } from '@ui/skeletons/ToolbarSkeleton'
 import styles from './Toolbar.module.css'
 
-// TODO: dynamic import with react-responsive / react-device-detect
+const ToolbarContent = dynamic(() => import('./ToolbarContent').then(mod => mod.ToolbarContent), {
+  ssr: false,
+  loading: () => <ToolbarContentSkeleton />
+})
+const ToolbarDropdownMenu = dynamic(
+  () => import('./ToolbarDropdownMenu').then(mod => mod.ToolbarDropdownMenu),
+  {
+    ssr: false,
+    loading: () => <ToolbarDropdownMenuSkeleton />
+  }
+)
+
 export const Toolbar = () => (
   <Flex px='3' align='center' justify='between' className={styles.root}>
     <ToolbarTabList />
-    <ToolbarContent className={styles.content} />
-    <ToolbarDropdownMenu triggerClassName={styles.dropdownMenuTrigger} />
+
+    <MediaQuery minWidth={520}>
+      <ToolbarContent />
+    </MediaQuery>
+
+    <MediaQuery maxWidth={519}>
+      <ToolbarDropdownMenu />
+    </MediaQuery>
   </Flex>
 )
