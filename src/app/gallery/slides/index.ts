266116d35125083762a2131f36adae61a8d57ec5
MiracleHorizon <alexeenkodenisjke@gmail.com>
2024-02-02 16:49:05 +0300

    Add new gallery slides

--- src/app/gallery/slides/index.ts
+++ src/app/gallery/slides/index.ts
@@ -4,8 +4,7 @@ import type GallerySlide from '../components/GallerySlide'
 
 // TODO: Унифицировать
 const githubRowPath = 'https://raw.githubusercontent.com'
-// TODO: Update before merge to main
-const staticFilePath = 'MiracleHorizon/scissors/feat/gallery/src/app/gallery/slides/'
+const staticFilePath = 'MiracleHorizon/scissors/main/src/app/gallery/slides/'
 const slidePath = `${githubRowPath}/${staticFilePath}/`
 
 type SlideProps = Omit<ComponentPropsWithoutRef<typeof GallerySlide>, 'index'>
@@ -55,6 +54,69 @@ export const slides: SlideProps[] = [
     }
   },
   {
+    label: 'Giza, Egypt',
+    beforeSrc: slidePath + 'slide-6-before.jpeg',
+    afterSrc: slidePath + 'slide-6-after.jpeg',
+    details: [
+      {
+        label: 'Grayscale',
+        value: 'enabled'
+      }
+    ],
+    settings: {
+      grayscale: true
+    }
+  },
+  {
+    label: 'Abstraction 1',
+    beforeSrc: slidePath + 'slide-4-before.jpeg',
+    afterSrc: slidePath + 'slide-4-after.jpeg',
+    details: [
+      {
+        label: 'Negate',
+        value: 'enabled'
+      }
+    ],
+    settings: {
+      negate: {
+        value: true,
+        alpha: false
+      }
+    },
+    orientation: 'portrait'
+  },
+  {
+    label: 'Blueberry',
+    beforeSrc: slidePath + 'slide-5-before.jpeg',
+    afterSrc: slidePath + 'slide-5-after.jpeg',
+    details: [
+      {
+        label: 'Negate',
+        value: 'enabled'
+      },
+      {
+        label: 'Tint',
+        value: '#2e2e2e'
+      },
+      {
+        label: 'Normalise',
+        value: '13-99%'
+      }
+    ],
+    settings: {
+      negate: {
+        value: true,
+        alpha: false
+      },
+      tint: '#2e2e2e',
+      normalise: {
+        lower: 13,
+        upper: 99
+      }
+    },
+    orientation: 'portrait'
+  },
+  {
     label: 'Sahara',
     beforeSrc: slidePath + 'slide-3-before.jpeg',
     afterSrc: slidePath + 'slide-3-after.jpeg',
@@ -72,5 +134,39 @@ export const slides: SlideProps[] = [
         saturation: null
       }
     }
+  },
+  {
+    // eslint-disable-next-line quotes
+    label: "The lion's leader",
+    beforeSrc: slidePath + 'slide-7-before.jpeg',
+    afterSrc: slidePath + 'slide-7-after.jpeg',
+    details: [
+      {
+        label: 'Grayscale',
+        value: 'enabled'
+      }
+    ],
+    settings: {
+      grayscale: true
+    },
+    orientation: 'portrait'
+  },
+  {
+    label: 'Jellyfish',
+    beforeSrc: slidePath + 'slide-9-before.jpeg',
+    afterSrc: slidePath + 'slide-9-after.jpeg',
+    details: [
+      {
+        label: 'Negate',
+        value: 'enabled'
+      }
+    ],
+    settings: {
+      negate: {
+        value: true,
+        alpha: false
+      }
+    },
+    orientation: 'portrait'
   }
 ]
