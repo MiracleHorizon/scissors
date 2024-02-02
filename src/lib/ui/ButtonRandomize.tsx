6984ca8a06b66bdc678066bdd3ff970414244ac9
MiracleHorizon <alexeenkodenisjke@gmail.com>
2024-02-02 17:09:55 +0300

    chore: update ButtonRandomize icon

--- src/lib/ui/ButtonRandomize.tsx
+++ src/lib/ui/ButtonRandomize.tsx
@@ -3,14 +3,14 @@
 import { forwardRef } from 'react'
 import { IconButton, Tooltip, type MarginProps } from '@radix-ui/themes'
 
-import { DiceSixIcon } from './icons/DiceSixIcon'
+import { ShuffleIcon } from './icons/ShuffleIcon'
 import type { ButtonProps } from '@lib/theme'
 
 export const ButtonRandomize = forwardRef<HTMLButtonElement, Props>(
   ({ tooltipContent, onClick, size = '2', variant = 'outline', ...props }, ref) => {
     const buttonJSX = (
       <IconButton ref={ref} {...props} size={size} variant={variant} onClick={onClick}>
-        <DiceSixIcon label='randomize' />
+        <ShuffleIcon width='18' height='18' label='randomize settings' />
       </IconButton>
     )
 
--- src/lib/ui/icons/ShuffleIcon.tsx
+++ src/lib/ui/icons/ShuffleIcon.tsx
@@ -3,8 +3,8 @@ import type { SVGProps } from 'react'
 
 import type { AccessibleIconProps } from '@lib/theme'
 
-export const DiceSixIcon = ({
-  label = 'dice six',
+export const ShuffleIcon = ({
+  label = 'shuffle',
   ...props
 }: SVGProps<SVGSVGElement> & AccessibleIconProps) => (
   <AccessibleIcon label={label}>
@@ -15,18 +15,16 @@ export const DiceSixIcon = ({
       viewBox='0 0 24 24'
       fill='none'
       stroke='currentColor'
-      strokeWidth='2'
+      strokeWidth='1.7'
       strokeLinecap='round'
       strokeLinejoin='round'
       {...props}
     >
-      <rect strokeWidth='1.3' width='18' height='18' x='3' y='3' rx='2' ry='2' />
-      <path d='M16 8h.01' />
-      <path d='M16 12h.01' />
-      <path d='M16 16h.01' />
-      <path d='M8 8h.01' />
-      <path d='M8 12h.01' />
-      <path d='M8 16h.01' />
+      <path d='M2 18h1.4c1.3 0 2.5-.6 3.3-1.7l6.1-8.6c.7-1.1 2-1.7 3.3-1.7H22' />
+      <path d='m18 2 4 4-4 4' />
+      <path d='M2 6h1.9c1.5 0 2.9.9 3.6 2.2' />
+      <path d='M22 18h-5.9c-1.3 0-2.6-.7-3.3-1.8l-.5-.8' />
+      <path d='m18 14 4 4-4 4' />
     </svg>
   </AccessibleIcon>
 )
