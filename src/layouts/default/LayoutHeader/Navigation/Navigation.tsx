94a43f525f655430039553d16ccce8f19b77ca58
MiracleHorizon <alexeenkodenisjke@gmail.com>
2024-02-02 15:40:30 +0300

    Add gallery page to the navigation

--- src/layouts/default/LayoutHeader/Navigation/Navigation.tsx
+++ src/layouts/default/LayoutHeader/Navigation/Navigation.tsx
@@ -5,7 +5,7 @@ import { ReaderIcon } from '@ui/icons/ReaderIcon'
 import { NavigationItem } from './NavigationItem'
 import { ROUTE } from '@lib/router'
 import { TOUR_STEP } from '@lib/tour'
-import type { Gap } from '@lib/theme'
+import { ImageIcon } from '@ui/icons/ImageIcon'
 
 const items: ComponentPropsWithoutRef<typeof NavigationItem>[] = [
   {
@@ -16,16 +16,17 @@ const items: ComponentPropsWithoutRef<typeof NavigationItem>[] = [
     // eslint-disable-next-line @typescript-eslint/ban-ts-comment
     // @ts-ignore
     'data-tourstep': TOUR_STEP.DOCUMENTATION_LINK
+  },
+  {
+    label: 'Gallery',
+    href: ROUTE.GALLERY,
+    tooltipContent: 'Watch the gallery',
+    icon: <ImageIcon width='18px' height='18px' label='go to gallery' />
   }
 ]
 
-const gap: Gap = {
-  initial: '2',
-  xs: '3'
-}
-
 export const Navigation = () => (
-  <Flex asChild align='center' gap={gap} mr='4'>
+  <Flex asChild align='center' gap='3' mr='4'>
     <nav>
       {items.map(item => (
         <NavigationItem key={item.label} {...item} />
