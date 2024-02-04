64d97642cd95c00be6f6ec5cf52176f376953fd0
MiracleHorizon <alexeenkodenisjke@gmail.com>
2024-02-04 19:51:54 +0300

    feat: add images for dark theme for  resize fit examples

--- src/widgets/SettingsPanel/TabResize/sections/Resize/ResizeExtra/ResizeSelectList/SelectResizeFit/ResizeFitExamplesPopover/ResizeFitExamplesPopover.tsx
+++ src/widgets/SettingsPanel/TabResize/sections/Resize/ResizeExtra/ResizeSelectList/SelectResizeFit/ResizeFitExamplesPopover/ResizeFitExamplesPopover.tsx
@@ -1,10 +1,57 @@
 import Image from 'next/image'
 import { Flex, type PaddingProps, Popover, ScrollArea, Text } from '@radix-ui/themes'
+import type { FC } from 'react'
 
 import { ButtonInfo } from '@ui/ButtonInfo'
 import { pathForPublic } from '@site/config'
 import type { FlexDirection, Gap } from '@lib/theme'
 import styles from './ResizeFitExamplesPopover.module.css'
+
+interface Example {
+  srcLight: string
+  srcDark: string
+  width: number
+  height: number
+  label: string
+}
+
+const examples: Example[] = [
+  {
+    srcLight: 'cover-light.png',
+    srcDark: 'cover-dark.png',
+    width: 360,
+    height: 264,
+    label: 'Cover'
+  },
+  {
+    srcLight: 'contain-light.png',
+    srcDark: 'contain-dark.png',
+    width: 264,
+    height: 264,
+    label: 'Contain'
+  },
+  {
+    srcLight: 'fill-light.png',
+    srcDark: 'fill-dark.png',
+    width: 264,
+    height: 264,
+    label: 'Fill'
+  },
+  {
+    srcLight: 'inside-light.png',
+    srcDark: 'inside-dark.png',
+    width: 264,
+    height: 264,
+    label: 'Inside'
+  },
+  {
+    srcLight: 'outside-light.png',
+    srcDark: 'outside-dark.png',
+    width: 360,
+    height: 264,
+    label: 'Outside'
+  }
+]
 
 function getExampleImagePath(fileName: string): string {
   const EXAMPLES_PUBLIC_PATH = 'resize-fit-examples'
@@ -12,38 +59,33 @@ function getExampleImagePath(fileName: string): string {
   return pathForPublic(`${EXAMPLES_PUBLIC_PATH}/${fileName}`)
 }
 
-const examples = [
-  {
-    src: getExampleImagePath('cover.png'),
-    width: 360,
-    height: 264,
-    label: 'Cover'
-  },
-  {
-    src: getExampleImagePath('contain.png'),
-    width: 264,
-    height: 264,
-    label: 'Contain'
-  },
-  {
-    src: getExampleImagePath('fill.png'),
-    width: 264,
-    height: 264,
-    label: 'Fill'
-  },
-  {
-    src: getExampleImagePath('inside.png'),
-    width: 264,
-    height: 264,
-    label: 'Inside'
-  },
-  {
-    src: getExampleImagePath('outside.png'),
-    width: 360,
-    height: 264,
-    label: 'Outside'
+const ASPECT_RATION_REDUCTION = 4.4
+
+const ResizeFitExampleImage: FC<Example> = ({ srcLight, srcDark, width, height, label }) => {
+  const common = {
+    alt: label,
+    width: width / ASPECT_RATION_REDUCTION,
+    height: height / ASPECT_RATION_REDUCTION
   }
-]
+
+  return (
+    <>
+      {/* eslint-disable-next-line jsx-a11y/alt-text */}
+      <Image
+        {...common}
+        src={getExampleImagePath(srcLight)}
+        className='resize-fit-example-image-light'
+      />
+      {/* eslint-disable-next-line jsx-a11y/alt-text */}
+      <Image
+        {...common}
+        src={getExampleImagePath(srcDark)}
+        className='resize-fit-example-image-dark'
+      />
+    </>
+  )
+}
+
 const exampleListStyle = {
   direction: {
     initial: 'column',
@@ -52,22 +94,15 @@ const exampleListStyle = {
   gap: {
     initial: '2',
     xs: '4'
-  } as Gap
-}
-const exampleListPadding: PaddingProps = {
-  pl: {
-    initial: '1',
-    xs: '2'
-  },
-  pt: {
-    initial: '1',
-    xs: '2'
-  },
-  pr: {
-    initial: '1',
-    xs: '2'
-  },
-  pb: '1'
+  } as Gap,
+  ...({
+    px: '2',
+    pb: '0',
+    pt: {
+      initial: '1',
+      xs: '2'
+    }
+  } as PaddingProps)
 }
 
 export const ResizeFitExamplesPopover = () => (
@@ -78,22 +113,11 @@ export const ResizeFitExamplesPopover = () => (
 
     <Popover.Content size='1' side='top' align='center' className={styles.content}>
       <ScrollArea type='scroll' scrollbars='both'>
-        <Flex
-          {...exampleListStyle}
-          {...exampleListPadding}
-          width='100%'
-          className={styles.exampleList}
-        >
-          {examples.map(({ width, height, label, ...image }) => (
-            <Flex
-              key={image.src}
-              direction='column'
-              align='center'
-              gap='1'
-              className={styles.exampleItem}
-            >
-              <Image width={width / 4.4} height={height / 4.4} alt={label} {...image} />
-              <Text as='span' size='2' weight='bold' className={styles.exampleLabel}>
+        <Flex {...exampleListStyle} width='100%' className={styles.exampleList}>
+          {examples.map(({ label, ...imageProps }) => (
+            <Flex key={label} direction='column' align='center' className={styles.exampleItem}>
+              <ResizeFitExampleImage label={label} {...imageProps} />
+              <Text as='span' size='2' weight='bold'>
                 {label}
               </Text>
             </Flex>
