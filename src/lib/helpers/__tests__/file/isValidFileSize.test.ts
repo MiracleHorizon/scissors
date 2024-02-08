bb24f7cc37cb9c09eeda56bd1b5afb7d02645e14
MiracleHorizon <alexeenkodenisjke@gmail.com>
2024-02-08 21:42:11 +0300

    refactor(helpers): isValidFileSize now takes file size instead of file object

--- src/lib/helpers/__tests__/file/isValidFileSize.test.ts
+++ src/lib/helpers/__tests__/file/isValidFileSize.test.ts
@@ -4,19 +4,19 @@ import { MAX_FILE_SIZE_MB } from '@site/config'
 
 describe('@lib/helpers/file/isValidFileSize', () => {
   it(`should return true if file size is less than ${MAX_FILE_SIZE_MB} MB`, () => {
-    expect(isValidFileSize({ size: 1.92 * BYTES_IN_MB } as File)).toBe(true)
+    expect(isValidFileSize(1.92 * BYTES_IN_MB)).toBe(true)
   })
 
   it(`should return true if file size is equal to ${MAX_FILE_SIZE_MB} MB`, () => {
-    expect(isValidFileSize({ size: MAX_FILE_SIZE } as File)).toBe(true)
+    expect(isValidFileSize(MAX_FILE_SIZE)).toBe(true)
   })
 
   it(`should return false if file size is greater than ${MAX_FILE_SIZE_MB} MB`, () => {
-    expect(isValidFileSize({ size: MAX_FILE_SIZE + 1 } as File)).toBe(false)
-    expect(isValidFileSize({ size: (MAX_FILE_SIZE_MB + 2) * BYTES_IN_MB } as File)).toBe(false)
+    expect(isValidFileSize(MAX_FILE_SIZE + 1)).toBe(false)
+    expect(isValidFileSize((MAX_FILE_SIZE_MB + 2) * BYTES_IN_MB)).toBe(false)
   })
 
   it('should return false if file size is less than 0', () => {
-    expect(isValidFileSize({ size: -1 } as File)).toBe(false)
+    expect(isValidFileSize(-1)).toBe(false)
   })
 })
