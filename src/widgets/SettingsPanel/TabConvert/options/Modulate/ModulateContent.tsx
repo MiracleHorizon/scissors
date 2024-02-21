bb3070c4ecd3750ad4282f4698493b8b7306927c
MiracleHorizon <alexeenkodenisjke@gmail.com>
2024-02-21 17:48:55 +0300

    renameeeeeeeeeeeeeeeeeeee.................

--- src/widgets/SettingsPanel/TabConvert/options/Modulate/ModulateContent.tsx
+++
@@ -1,9 +0,0 @@
-import { ModulateHeader } from './ModulateHeader'
-import { ModulateList } from './ModulateList'
-
-export const ModulateContent = () => (
-  <>
-    <ModulateHeader />
-    <ModulateList />
-  </>
-)
--- src/widgets/SettingsPanel/TabConvert/options/Modulate/index.ts
+++
@@ -1,1 +0,0 @@
-export { Modulate } from './Modulate'
