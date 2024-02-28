30e282a9d0a2ee9d4d9f6e079c434cf22a3f94e1
MiracleHorizon <alexeenkodenisjke@gmail.com>
2024-02-28 06:31:12 +0300

    fix(frontend): update path assets path
    [skip ci]

--- apps/frontend/src/site/config.ts
+++ apps/frontend/src/site/config.ts
@@ -55,6 +55,7 @@ const REPO_OWNER = 'MiracleHorizon'
 const REPO_NAME = 'scissors'
 const REPO_BRANCH = 'main'
 const REPO_PATH = `${REPO_OWNER}/${REPO_NAME}`
+const FRONTEND_PATH = 'apps/frontend'
 
 const GITHUB_USER_CONTENT_PATH = 'raw.githubusercontent.com'
 const GITHUB_USER_CONTENT_REPO_PATH = `https://${GITHUB_USER_CONTENT_PATH}/${REPO_PATH}/${REPO_BRANCH}`
@@ -65,7 +66,7 @@ export const GITHUB_REPO_PATH = `https://${GITHUB_PATH}/${REPO_PATH}`
 export function pathForAssets(path: string): string {
   const ASSETS_PATH = 'src/assets'
 
-  return `${GITHUB_USER_CONTENT_REPO_PATH}/${ASSETS_PATH}/${path}`
+  return `${GITHUB_USER_CONTENT_REPO_PATH}/${FRONTEND_PATH}/${ASSETS_PATH}/${path}`
 }
 
 export function pathForSocial(path: string): string {
