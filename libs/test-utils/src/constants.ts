6c6acd0736bd0ca57b4f04ecd43cead1bd7903cc
MiracleHorizon <alexeenkodenisjke@gmail.com>
2024-03-21 23:50:34 +0300

    feat: add test-utils lib

---
+++ libs/test-utils/src/constants.ts
@@ -0,0 +1,100 @@
+import {
+  type ConvertSettings,
+  DEFAULT_GAMMA,
+  EXTEND_WITH,
+  IMAGE_FILE_FORMAT,
+  MAX_NORMALISE,
+  MIN_BRIGHTNESS,
+  MIN_EXTEND_DIRECTION_SIZE,
+  MIN_HUE,
+  MIN_LIGHTNESS,
+  MIN_NORMALISE,
+  MIN_RESIZE_SIZE,
+  MIN_SATURATION,
+  MIN_TRIM_THRESHOLD,
+  RESIZE_FIT,
+  RESIZE_KERNEL,
+  RESIZE_POSITION,
+  type ResizeSettings
+} from '@scissors/sharp'
+
+const VALID_CONVERT_SETTINGS: ConvertSettings = {
+  flip: true,
+  flop: false,
+  grayscale: true,
+  negate: {
+    value: true,
+    alpha: false
+  },
+  normalise: {
+    lower: MIN_NORMALISE,
+    upper: MAX_NORMALISE
+  },
+  blur: {
+    value: true,
+    sigma: 1
+  },
+  rotate: {
+    angle: 90,
+    background: '#FFFFFF',
+    withDominantBackground: false
+  },
+  gamma: DEFAULT_GAMMA,
+  tint: '#FF0004',
+  modulate: {
+    lightness: MIN_LIGHTNESS,
+    brightness: MIN_BRIGHTNESS,
+    saturation: MIN_SATURATION,
+    hue: MIN_HUE
+  },
+  outputFormat: IMAGE_FILE_FORMAT.PNG
+} as const
+const VALID_RESIZE_SETTINGS: ResizeSettings = {
+  queue: [
+    {
+      name: 'resize',
+      queueIndex: 0
+    },
+    {
+      name: 'trim',
+      queueIndex: 1
+    }
+  ],
+  resize: {
+    width: MIN_RESIZE_SIZE,
+    height: MIN_RESIZE_SIZE,
+    fit: RESIZE_FIT.CONTAIN,
+    background: '#000000',
+    position: RESIZE_POSITION.CENTER,
+    kernel: RESIZE_KERNEL.LANCZOS3,
+    withoutEnlargement: true,
+    withoutReduction: false,
+    fastShrinkOnLoad: true,
+    withDominantBackground: false
+  },
+  extend: {
+    top: MIN_EXTEND_DIRECTION_SIZE,
+    bottom: MIN_EXTEND_DIRECTION_SIZE,
+    right: MIN_EXTEND_DIRECTION_SIZE,
+    left: MIN_EXTEND_DIRECTION_SIZE,
+    extendWith: EXTEND_WITH.BACKGROUND,
+    background: '#FFFFFF',
+    withDominantBackground: false
+  },
+  trim: {
+    background: '#000000',
+    threshold: MIN_TRIM_THRESHOLD,
+    lineArt: false
+  },
+  extract: {
+    left: 10,
+    top: 10,
+    width: 100,
+    height: 100
+  }
+} as const
+
+export {
+  VALID_CONVERT_SETTINGS as validConvertSettings,
+  VALID_RESIZE_SETTINGS as validResizeSettings
+}
---
+++ libs/test-utils/src/mock-generators.ts
@@ -0,0 +1,44 @@
+import {
+  type ConvertSettings,
+  MAX_NORMALISE,
+  MAX_ROTATE_ANGLE,
+  MIN_NORMALISE,
+  type ResizeSettings
+} from '@scissors/sharp'
+
+import { validConvertSettings, validResizeSettings } from './constants'
+
+// TODO: REWORK
+export function generateInvalidConvertSettings(): ConvertSettings {
+  return {
+    ...validConvertSettings,
+    outputFormat: '.foo',
+    normalise: {
+      lower: MIN_NORMALISE - 1,
+      upper: MAX_NORMALISE + 1
+    },
+    rotate: {
+      ...validConvertSettings.rotate,
+      angle: MAX_ROTATE_ANGLE + 1
+    }
+  } as unknown as ConvertSettings
+}
+
+export function generateInvalidResizeSettings(): ResizeSettings {
+  return {
+    ...validResizeSettings,
+    extend: {
+      ...validResizeSettings.extend!,
+      right: -1
+    },
+    resize: {
+      ...validResizeSettings.resize!,
+      background: {
+        r: 0,
+        g: 0,
+        b: 0,
+        a: 0
+      }
+    }
+  } as unknown as ResizeSettings
+}
