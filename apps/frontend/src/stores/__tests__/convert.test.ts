ebbf90da8cec4eb6b8fe71b784892984dc661e30
MiracleHorizon <alexeenkodenisjke@gmail.com>
2024-04-01 19:11:52 +0300

    test(frontend): add tests for convert store

---
+++ apps/frontend/src/stores/__tests__/convert.test.ts
@@ -0,0 +1,56 @@
+// @vitest-environment jsdom
+
+import { createConvertStore, defaultState } from '@stores/convert'
+
+describe('@stores/convert', () => {
+  it('should correctly set options values', () => {
+    const store = createConvertStore()
+
+    expect(store.getState().flip).toBe(defaultState.flip)
+    store.getState().setFlip(!defaultState.flip)
+    expect(store.getState().flip).toBe(!defaultState.flip)
+
+    expect(store.getState().flop).toBe(defaultState.flop)
+    store.getState().setFlop(!defaultState.flop)
+    expect(store.getState().flop).toBe(!defaultState.flop)
+    store.getState().setFlop(defaultState.flop)
+    expect(store.getState().flop).toBe(defaultState.flop)
+
+    expect(store.getState().grayscale).toBe(defaultState.grayscale)
+    store.getState().setGrayscale(!defaultState.grayscale)
+    expect(store.getState().grayscale).toBe(!defaultState.grayscale)
+  })
+
+  it('should correctly toggle options values', () => {
+    const store = createConvertStore()
+
+    expect(store.getState().flip).toBe(defaultState.flip)
+    store.getState().toggleFlip()
+    expect(store.getState().flip).toBe(!defaultState.flip)
+
+    expect(store.getState().flop).toBe(defaultState.flop)
+    store.getState().toggleFlop()
+    expect(store.getState().flop).toBe(!defaultState.flop)
+
+    expect(store.getState().grayscale).toBe(defaultState.grayscale)
+    store.getState().toggleGrayscale()
+    expect(store.getState().grayscale).toBe(!defaultState.grayscale)
+  })
+
+  it('should correctly reset state', () => {
+    const store = createConvertStore()
+
+    store.getState().setFlip(true)
+    store.getState().setFlop(false)
+    store.getState().setGrayscale(true)
+
+    expect(store.getState().getConvertSettings()).toStrictEqual({
+      flip: true,
+      flop: false,
+      grayscale: true
+    })
+
+    store.getState().reset()
+    expect(store.getState().getConvertSettings()).toStrictEqual(defaultState)
+  })
+})
