25e89b0d62accb782befbd94b792025f6df5cbf2
MiracleHorizon <alexeenkodenisjke@gmail.com>
2024-07-25 23:37:54 +0300

    fix(frontend): getRandomNumber

--- apps/frontend/src/lib/helpers/getRandomNumber.ts
+++ apps/frontend/src/lib/helpers/getRandomNumber.ts
@@ -1,8 +1,6 @@
-import random from 'lodash.random'
-
 export const getRandomNumber = (min: number, max: number, precision?: number): number => {
   const floating = typeof precision === 'number'
-  let randomValue = random(min, max, floating)
+  let randomValue = Math.random() * (max - min) + min
 
   if (floating) {
     randomValue = parseFloat(randomValue.toPrecision(precision))
