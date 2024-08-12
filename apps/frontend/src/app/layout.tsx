e4fedfdeb5a371701d091e65b5e77ae17e97b68d
MiracleHorizon <alexeenkodenisjke@gmail.com>
2024-08-13 00:23:15 +0300

    feat(frontend): add basic Yandex.Metrika setup

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -37,6 +37,35 @@ const RootLayout = async ({ children }: PropsWithChildren) => {
 
         <Analytics debug={false} />
         <SpeedInsights debug={false} />
+
+        <script defer>
+          {`
+            ;(function (m, e, t, r, i, k, a) {
+              m[i] =
+                m[i] ||
+                function () {
+                  ;(m[i].a = m[i].a || []).push(arguments)
+                }
+              m[i].l = 1 * new Date()
+              for (var j = 0; j < document.scripts.length; j++) {
+                if (document.scripts[j].src === r) {
+                  return
+                }
+              }
+              ;(k = e.createElement(t)),
+                (a = e.getElementsByTagName(t)[0]),
+                (k.async = 1),
+                (k.src = r),
+                a.parentNode.insertBefore(k, a)
+            })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym')
+
+            ym(98055329, 'init', {
+              clickmap: true,
+              trackLinks: true,
+              accurateTrackBounce: true
+            })
+          `}
+        </script>
       </body>
     </html>
   )
