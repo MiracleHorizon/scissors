060628a84a6e6f6c4b494aab5f70bdbd54e73bc2
MiracleHorizon <alexeenkodenisjke@gmail.com>
2024-08-13 00:49:03 +0300

    feat(frontend): add basic Yandex.Metrika setup

--- apps/frontend/src/app/layout.tsx
+++ apps/frontend/src/app/layout.tsx
@@ -38,34 +38,20 @@ const RootLayout = async ({ children }: PropsWithChildren) => {
         <Analytics debug={false} />
         <SpeedInsights debug={false} />
 
-        <script defer>
-          {`
-            ;(function (m, e, t, r, i, k, a) {
-              m[i] =
-                m[i] ||
-                function () {
-                  ;(m[i].a = m[i].a || []).push(arguments)
-                }
-              m[i].l = 1 * new Date()
-              for (var j = 0; j < document.scripts.length; j++) {
-                if (document.scripts[j].src === r) {
-                  return
-                }
-              }
-              ;(k = e.createElement(t)),
-                (a = e.getElementsByTagName(t)[0]),
-                (k.async = 1),
-                (k.src = r),
-                a.parentNode.insertBefore(k, a)
-            })(window, document, 'script', 'https://mc.yandex.ru/metrika/tag.js', 'ym')
+        <script defer src='/yandex-metrika.js' type='module' crossOrigin='anonymous' />
 
-            ym(98055329, 'init', {
-              clickmap: true,
-              trackLinks: true,
-              accurateTrackBounce: true
-            })
-          `}
-        </script>
+        <noscript>
+          <div>
+            <img
+              src='https://mc.yandex.ru/watch/98055329'
+              style={{
+                position: 'absolute',
+                left: '-9999px'
+              }}
+              alt=''
+            />
+          </div>
+        </noscript>
       </body>
     </html>
   )
