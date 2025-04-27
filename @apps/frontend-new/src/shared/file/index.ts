fe66ecd24525bd3e6bc3a1425b7135380d0a82ec
MiracleHorizon <alexeenkodenisjke@gmail.com>
2025-04-28 02:36:32 +0300

    feat(file): add file validation utilities including name, size, and type checks

---
+++ @apps/frontend-new/src/shared/file/index.ts
@@ -0,0 +1,4 @@
+export { isValidFileName } from './isValidFileName'
+export { isValidFileSize } from './isValidFileSize'
+export { isValidFileType } from './isValidFileType'
+export * from './constants'
