cf2c58577e7ec064e587cc8b503f7b8c80b906b1
MiracleHorizon <alexeenkodenisjke@gmail.com>
2025-05-07 17:47:51 +0300

    refactor: update API route definitions and enhance error handling
    - Changed API route definitions in @apps/ai, @apps/processor, and @apps/s3-bridge to use object syntax for better clarity.
    - Improved error handling for requests in the completion and conversion endpoints.
    - Added support for OPTIONS method in CORS handling to comply with preflight requests.

--- @apps/ai/src/index.ts
+++ @apps/ai/src/index.ts
@@ -35,112 +35,119 @@ setInterval(() => {
 }, RESET_TIME_INTERVAL)
 
 // TODO: Logger
+// TODO: server.requestIP
 serve({
   port: PORT,
   routes: injectCORS(
     {
-      '/api/v1/completion': async req => {
-        // TODO: Нужен какой-то алерт в ТГ (etc), что кто-то пытается абузить
-        if (totalRequests >= MAX_REQUESTS_PER_HOUR) {
-          return Response.json(
-            {
-              message: 'Rate limit exceeded'
-            },
-            { status: 429 }
-          )
-        }
-
-        const { prompt } = await req.json()
-
-        if (!prompt) {
-          return Response.json(
-            {
-              message: 'Prompt is required'
-            },
-            {
-              status: 400
-            }
-          )
-        }
-
-        try {
-          const url = 'https://llm.api.cloud.yandex.net/foundationModels/v1/completion'
-          const response = await fetch(url, {
-            method: 'POST',
-            headers: {
-              Authorization: `Api-Key ${YA_GPT_API_KEY}`,
-              'Content-Type': 'application/json',
-              'x-folder-id': YA_GPT_FOLDER_ID
-            },
-            body: JSON.stringify({
-              modelUri: `gpt://${YA_GPT_FOLDER_ID}/${YA_GPT_MODEL}`,
-              completionOptions: {
-                stream: false,
-                temperature: 0.6,
-                maxTokens: '2000'
+      '/api/v1/completion': {
+        POST: async req => {
+          // TODO: Нужен какой-то алерт в ТГ (etc), что кто-то пытается абузить
+          if (totalRequests >= MAX_REQUESTS_PER_HOUR) {
+            return Response.json(
+              {
+                message: 'Rate limit exceeded'
               },
-              messages: [
-                {
-                  role: 'user',
-                  text: getContextForPrompt(prompt)
-                }
-              ]
+              {
+                status: 429
+              }
+            )
+          }
+
+          const { prompt } = await req.json()
+
+          if (!prompt) {
+            return Response.json(
+              {
+                message: 'Prompt is required'
+              },
+              {
+                status: 400
+              }
+            )
+          }
+
+          try {
+            const url = 'https://llm.api.cloud.yandex.net/foundationModels/v1/completion'
+            const response = await fetch(url, {
+              method: 'POST',
+              headers: {
+                Authorization: `Api-Key ${YA_GPT_API_KEY}`,
+                'Content-Type': 'application/json',
+                'x-folder-id': YA_GPT_FOLDER_ID
+              },
+              body: JSON.stringify({
+                modelUri: `gpt://${YA_GPT_FOLDER_ID}/${YA_GPT_MODEL}`,
+                completionOptions: {
+                  stream: false,
+                  temperature: 0.6,
+                  maxTokens: '2000'
+                },
+                messages: [
+                  {
+                    role: 'user',
+                    text: getContextForPrompt(prompt)
+                  }
+                ]
+              })
             })
-          })
 
-          if (!response.ok) {
-            console.error(
-              `[AI] [api/v1/completion] Bad request: ${response.status}: "${response.statusText}"`
-            )
+            if (!response.ok) {
+              console.error(
+                `[AI] [api/v1/completion] Bad request: ${response.status}: "${response.statusText}"`
+              )
 
-            return Response.json(
-              {
-                message: 'Bad request',
-                error: response.statusText
-              },
-              {
-                status: 400
-              }
-            )
-          }
-
-          const { result } = await response.json()
-          const alternatives: { message: YaGptMessage }[] = result.alternatives
-          const messages = alternatives.map(({ message }) => message.text)
-          totalRequests++
-
-          if (messages.some(message => message.toLowerCase().startsWith('invalid'))) {
-            return Response.json(
-              {
-                message: 'WRONG_DATA'
-              },
-              {
-                status: 400
-              }
-            )
-          }
-
-          const settings = parseSettingsFromGpt(messages[0])
-
-          return Response.json(settings)
-        } catch (error) {
-          console.error('[AI] [api/v1/completion] Error: ', error)
-
-          if (error instanceof Error) {
-            return Response.json(
-              {
-                message: 'Something went wrong'
-              },
-              { status: 500 }
-            )
-          }
-
-          return Response.json(
-            {},
-            {
-              status: 500
+              return Response.json(
+                {
+                  message: 'Bad request',
+                  error: response.statusText
+                },
+                {
+                  status: 400
+                }
+              )
             }
-          )
+
+            const { result } = await response.json()
+            const alternatives: { message: YaGptMessage }[] = result.alternatives
+            const messages = alternatives.map(({ message }) => message.text)
+            totalRequests++
+
+            if (messages.some(message => message.toLowerCase().startsWith('invalid'))) {
+              return Response.json(
+                {
+                  message: 'WRONG_DATA'
+                },
+                {
+                  status: 400
+                }
+              )
+            }
+
+            const settings = parseSettingsFromGpt(messages[0])
+
+            return Response.json(settings)
+          } catch (error) {
+            console.error('[AI] [api/v1/completion] Error: ', error)
+
+            if (error instanceof Error) {
+              return Response.json(
+                {
+                  message: 'Something went wrong'
+                },
+                {
+                  status: 500
+                }
+              )
+            }
+
+            return Response.json(
+              {},
+              {
+                status: 500
+              }
+            )
+          }
         }
       }
     },
@@ -149,5 +156,11 @@ serve({
       methods: ['POST'],
       credentials: false
     }
-  )
+  ),
+  fetch: () =>
+    new Response('Not Found', {
+      status: 404
+    })
 })
+
+console.log(`[AI] Server is running on port ${PORT}`)
--- @packages/bun-cors/src/types.ts
+++ @packages/bun-cors/src/types.ts
@@ -1,4 +1,4 @@
-export type HttpMethod = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE'
+export type HttpMethod = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE' | 'OPTIONS'
 
 export interface CorsOptions {
   origin?: string
